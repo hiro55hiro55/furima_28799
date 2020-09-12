8385463cbee362199586597d0cb6f666924a6114
hiro55hiro55 <kiti-kiti@hotmail.co.jp>
2020-09-12 15:33:06 +0900

    ルーティングのネスト

---
+++ db/migrate/20200910051540_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+
+      t.integer :postal_code
+      t.string  :city
+      t.string :addresses
+      t.string :phone_number
+      t.integer :shop_id, foreign_key: true
+      t.integer :address_prefecture_id
+      t.string :buildings
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_065452) do
+ActiveRecord::Schema.define(version: 2020_09_10_051540) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,18 @@ ActiveRecord::Schema.define(version: 2020_09_07_065452) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "postal_code"
+    t.string "city"
+    t.string "addresses"
+    t.string "phone_number"
+    t.integer "shop_id"
+    t.integer "address_prefecture_id"
+    t.string "buildings"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
